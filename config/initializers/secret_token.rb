5d091bccee36548da82b377887777ea0c6a28add
Ubuntu x64 <jose@ubuntu.(none)>
2012-11-17 21:49:29 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NueventoApp::Application.config.secret_token = '2a56b3e5924cb56949a33fc7a476cf31e6711dcba2367f287bffa895c60efd384db49384e4ca848638a9fddfcce1411d37e192a2492299756a7e3cbb44ff9c3f'
